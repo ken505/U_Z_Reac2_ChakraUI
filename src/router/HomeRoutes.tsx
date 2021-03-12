0a1525c684c7eeb1d3f66eda1d24b9f129976796
ken505 <68226398+ken505@users.noreply.github.com>
2021-03-12 21:44:43 +0900

    66_wip:

--- src/router/HomeRoutes.tsx
+++ src/router/HomeRoutes.tsx
@@ -1,6 +1,6 @@
 import { Home } from "../components/pages/Home";
 import { Setting } from "../components/pages/Setting";
-import { UserManagement } from "../components/UserManagement";
+import { UserManagement } from "../components/pages/UserManagement";
 
 export const homeRoutes = [
   {
@@ -9,7 +9,7 @@ export const homeRoutes = [
     children: <Home />
   },
   {
-    path: "/user_mamagement",
+    path: "/user_management",
     exact: false,
     children: <UserManagement />
   },
